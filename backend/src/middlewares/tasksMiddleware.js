1f1c9d4667f7edc719e64d8ea0925a1224618361
Fabiel Avela <fabielbatista@hotmail.com>
2023-06-01 23:08:24 -0300

    Finalizado

---
+++ backend/src/middlewares/tasksMiddleware.js
@@ -0,0 +1,33 @@
+const validateFieldTitle = (request, response, next) => {
+    const { body } = request;
+
+    if (body.title == undefined) {
+        response.status(400).json({message:'The field "title" is required'});
+    }
+
+    if (body.title == '') {
+        response.status(400).json({message:'title cannot be empty'});
+    }
+    next();
+};
+
+    const validateFieldStatus = (request, response, next) => {
+        const { body } = request;
+    
+        if (body.status== undefined) {
+            response.status(400).json({message:'The field "title" is required'});
+        }
+    
+        if (body.status == '') {
+            response.status(400).json({message:'title cannot be empty'});
+        }
+
+    
+
+    next();
+    };
+
+module.exports = {
+    validateFieldTitle,
+    validateFieldStatus,
+};
